f048a3958d327bfb73a34fe586548b6ffd52e74f
Vitor Hugo Nakai <36956647+vhnakai@users.noreply.github.com>
2023-06-03 21:19:49 -0300

    Add new Routes

---
+++ src/server/api/routers/blocked-dates.ts
@@ -0,0 +1,65 @@
+import { z } from "zod";
+
+import {
+  createTRPCRouter,
+  publicProcedure,
+  //protectedProcedure,
+} from "@/server/api/trpc";
+
+export const blockedDatesrouter = createTRPCRouter({
+
+  blockedDates: publicProcedure
+    .input(z.object({
+      user_uuid: z.string(),
+      year: z.number(),
+      month: z.number(),
+    }))
+    .query(async ({ ctx, input }) => {
+
+      const { user_uuid, year, month } = input
+
+      const availableWeekDays = await ctx.prisma.userTimeInterval.findMany({
+        select: {
+          week_day: true,
+        },
+        where: {
+          userId: user_uuid,
+        },
+      })
+
+      const blockedWeekDays = [0, 1, 2, 3, 4, 5, 6].filter((weekDay) => {
+        return !availableWeekDays.some(
+          (availableWeekDay) => availableWeekDay.week_day === weekDay,
+        )
+      })
+
+
+      const blockedDatesRaw: Array<{ date: number }> = await ctx.prisma.$queryRaw`
+        SELECT
+          EXTRACT(DAY FROM S.DATE) AS date,
+          COUNT(S.date) AS amount,
+          ((UTI.time_end_in_minutes - UTI.time_start_in_minutes) / 60) AS size
+
+        FROM schedulings S
+
+        LEFT JOIN user_time_intervals UTI
+          ON UTI.week_day = WEEKDAY(DATE_ADD(S.date, INTERVAL 1 DAY))
+
+        WHERE S.userId = ${user_uuid}
+          AND DATE_FORMAT(S.date, "%Y-%m") = ${`${year}-${month}`}
+
+        GROUP BY EXTRACT(DAY FROM S.DATE),
+          ((UTI.time_end_in_minutes - UTI.time_start_in_minutes) / 60)
+
+        HAVING amount >= size
+      `
+
+      const blockedDates = blockedDatesRaw.map((item) => item.date)
+
+      return { blockedWeekDays, blockedDates }
+    }),
+
+  // getSecretMessage: protectedProcedure.query(() => {
+  //   return "you can now see this secret message!";
+  // }),
+});
---
+++ src/server/api/routers/schedurie.ts
@@ -0,0 +1,90 @@
+import { z } from "zod";
+import dayjs from "dayjs";
+
+import {
+  createTRPCRouter,
+  publicProcedure,
+  //protectedProcedure,
+} from "@/server/api/trpc";
+
+export const scheduringRouter = createTRPCRouter({
+
+  schedure: publicProcedure
+    .input(z.object({
+      user_uuid: z.string(),
+      name: z.string(),
+      email: z.string().email(),
+      observations: z.string(),
+      date: z.string().datetime(),
+    }))
+    .query(async ({ ctx, input }) => {
+
+      const { user_uuid, name, email, observations, date } = input
+
+      const schedulingDate = dayjs(date).startOf('hour')
+
+      if (schedulingDate.isBefore(new Date())) {
+        return {
+          message: 'Date is in the past.',
+        }
+      }
+
+      const conflictingScheduling = await ctx.prisma.scheduling.findFirst({
+        where: {
+          userId: user_uuid,
+          date: schedulingDate.toDate(),
+        },
+      })
+
+      if (conflictingScheduling) {
+        return {
+          message: 'There is another scheduling at at the same time.',
+        }
+      }
+
+      const scheduling = await ctx.prisma.scheduling.create({
+        data: {
+          name,
+          email,
+          observations,
+          date: schedulingDate.toDate(),
+          userId: user_uuid,
+        },
+      })
+
+      // const calendar = google.calendar({
+      //   version: 'v3',
+      //   auth: await getGoogleOAuthToken(user.id),
+      // })
+
+      // await calendar.events.insert({
+      //   calendarId: 'primary',
+      //   conferenceDataVersion: 1,
+      //   requestBody: {
+      //     summary: `Consulta: ${name}`,
+      //     description: observations,
+      //     start: {
+      //       dateTime: schedulingDate.format(),
+      //     },
+      //     end: {
+      //       dateTime: schedulingDate.add(1, 'hour').format(),
+      //     },
+      //     attendees: [{ email, displayName: name }],
+      //     conferenceData: {
+      //       createRequest: {
+      //         requestId: scheduling.id,
+      //         conferenceSolutionKey: {
+      //           type: 'hangoutsMeet',
+      //         },
+      //       },
+      //     },
+      //   },
+      // })
+
+      return { message: 'A new appointment was successfully scheduled.', schedureId: scheduling.id  }
+    }),
+
+  // getSecretMessage: protectedProcedure.query(() => {
+  //   return "you can now see this secret message!";
+  // }),
+});
