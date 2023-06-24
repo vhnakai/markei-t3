e1a7ff234559cb30c3a753bd740723378faff19c
Vitor Hugo Nakai <36956647+vhnakai@users.noreply.github.com>
2023-06-24 13:57:57 -0300

    New components

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -6,6 +6,16 @@
  * */
 
 import { Calendar } from '@/components/ui/calendar'
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table'
+
 import { api } from '@/utils/api'
 import { useUser } from '@clerk/nextjs'
 import { ptBR } from 'date-fns/locale'
@@ -14,7 +24,7 @@ import { type NextPage } from 'next'
 import Head from 'next/head'
 import { useState } from 'react'
 
-const AppointmentList = (props: { userId: string }) => {
+const AppointmentTable = (props: { userId: string }) => {
   const { data: appointments } = api.schedure.appointments.useQuery({
     userUuid: props.userId,
   })
@@ -22,19 +32,32 @@ const AppointmentList = (props: { userId: string }) => {
   if (!appointments) return <h1>No apppointment was found</h1>
 
   return (
-    <div className="flex flex-col">
-      {appointments.map((appointment) => (
-        <div
-          key={appointment.name}
-          className="mt-2 rounded-lg border-4 border-border p-3"
-        >
-          <span className="text-md">
-            {appointment.name} - {dayjs(appointment.date).format('DD/MM/YYYY')}{' '}
-            - {appointment.observations}
-          </span>
-        </div>
-      ))}
-    </div>
+    <Table>
+      <TableCaption>
+        Seus agendamentos marcados para hoje (
+        {dayjs(new Date()).format('DD/MM/YYYY')} )
+      </TableCaption>
+      <TableHeader>
+        <TableRow>
+          <TableHead className="w-[100px]">Nome</TableHead>
+          <TableHead>Status</TableHead>
+          <TableHead>Horário</TableHead>
+          <TableHead className="text-right">Ação</TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        {appointments.map((appointment) => (
+          <TableRow key={appointment.name}>
+            <TableCell className="font-medium">{appointment.name}</TableCell>
+            <TableCell>{appointment.observations}</TableCell>
+            <TableCell>
+              {dayjs(appointment.date).format('DD/MM/YYYY')}
+            </TableCell>
+            <TableCell className="text-right">Botao</TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
   )
 }
 
@@ -55,8 +78,8 @@ const DashboardPage: NextPage = () => {
       <Head>
         <title>{user.username ?? user.firstName}</title>
       </Head>
-      <div className="flex max-w-full items-center justify-center ">
-        <AppointmentList userId={user.id} />
+      <div className="flex max-w-full items-stretch justify-center ">
+        <AppointmentTable userId={user.id} />
 
         <Calendar
           mode="single"
