c6099ac9960b4f197faafbcc45f23e672881eb2c
Vitor Hugo Nakai <36956647+vhnakai@users.noreply.github.com>
2023-06-30 11:05:21 -0300

    Fix

--- src/utils/convert-time-string-to-minute.ts
+++ src/utils/convert-time-string-to-minute.ts
@@ -1,6 +1,5 @@
-/* eslint-disable @typescript-eslint/restrict-plus-operands */
 export function convertTimeStringToMinutes(timeString: string) {
   const [hours, minutes] = timeString.split(':').map(Number)
 
-  return hours * 60 + minutes
+  return Number(hours) * 60 + Number(minutes)
 }
